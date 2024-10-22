102fc9b572a22e7bae9c9407bc58815e5070e10a
leahmathena <leahashleymann@gmail.com>
2024-10-21 20:21:02 -0500

    removing app.py from static

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-APP.PY :: from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import plotly.express as px
 from pymongo import MongoClient
